ce7f50caaa9129ea90dd52fa46b48204995ee763
taivic <vicky.yen.tai@gmail.com>
2016-08-13 10:25:38 -0500

    sound update

--- js/app.js
+++ js/app.js
@@ -8,15 +8,26 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$('.hadouken').hide();
+				$('.hadouken').css('left', '520px');
+			});
 	})
 	.mouseup(function(){
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-	})
-
-		
+	});
 
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
