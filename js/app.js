8d4819c02ab2cebe00f0a91b476fe119055e0ccb
taivic <vicky.yen.tai@gmail.com>
2016-08-13 09:45:55 -0500

    modified js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+	})
+	.mouseup(function(){
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	})
+
+		
+
+});
